361e506a7a18325dc5ad43c1e94daa5609db440d
Aaron <aaronjacobespinosa54@gmail.com>
2026-06-08 00:06:32 +0800

    Boolean prints and fixed nextLine

--- Pageant.java
+++ Pageant.java
@@ -4,35 +4,32 @@ public class Pageant{
     public static void main(String[] args){
         Contestant[] contestants = new Contestant[5]; //Array of 5 contestant objects (should be freed).
         Scanner sc = new Scanner(System.in); 
-        boolean canCompete = false; //If contestant can compete in singing.
+        boolean canSing = false; //If contestant can compete in singing.
+        boolean isOld = false; 
 
         //Loops 5 times to input contestant information.
         for(int i = 0 ;i<contestants.length; i++){
             System.out.println("Please input name of contestant.");
-            String n = sc.next();
+            String n = sc.nextLine();
             System.out.println("Please input country of contestant.");
-            String c = sc.next();
+            String c = sc.nextLine();
             System.out.println("Please input age of contestant.");
             int a = sc.nextInt();
+            sc.nextLine();
             contestants[i] = new Contestant(n,c,a);
         }
 
         //input 2 talents and input into contestant 1
         System.out.println("Please input talent 1 of contestant 1.");
-        contestants[0].addTalent(sc.next()); 
+        contestants[0].addTalent(sc.nextLine()); 
         System.out.println("Please input talent 2 of contestant 1.");
-        contestants[0].addTalent(sc.next());
+        contestants[0].addTalent(sc.nextLine());
 
-        if(contestants[0].canCompete("singing")){
-            canCompete = true;
-        }
+        canSing = contestants[0].canCompete("singing");
+        isOld = contestants[0].canCompete("singing", 25);
+        boolean isQual = canSing && isOld;
 
-        if(contestants[0].canCompete(25)){
-            System.out.println("Can" + " " + contestants[0].getName() + " " + "compete in singing : " + canCompete);
-        }
-        else{
-            System.out.println(contestants[0].getName() + " is not old enough to compete in singing.");
-        }
+        System.out.println("Can "+ contestants[0].getName() + " compete in singing and is old enough? : " + isQual);
 
         contestants = null;
         sc.close();
