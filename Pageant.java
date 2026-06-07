8ed0819dc0966ba235aa6cc7b0806218e42ddd69
Aaron <aaronjacobespinosa54@gmail.com>
2026-06-07 08:33:37 +0800

    Driver is compiling, with issues on object

--- Pageant.java
+++ Pageant.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Pageant{
     public static void main(String[] args){
-        Contestant[5] contestants;
-        Scanner sc = Scanner(System.in);
-        boolean canCompete = false;
+        Contestant[] contestants = new Contestant[5]; //Array of 5 contestant objects (should be freed).
+        Scanner sc = new Scanner(System.in); 
+        boolean canCompete = false; //If contestant can compete in singing.
 
+        //Loops 5 times to input contestant information.
         for(int i = 0 ;i<contestants.length; i++){
             System.out.println("Please input name of contestant.");
             String n = sc.next();
@@ -13,19 +14,22 @@ public class Pageant{
             String c = sc.next();
             System.out.println("Please input age of contestant.");
             int a = sc.nextInt();
-            contestsants[i] = new Contestant(n,c,a);
+            contestants[i] = new Contestant(n,c,a);
         }
-        //input 2 talents and input into contestant 0
+
+        //input 2 talents and input into contestant 1
         System.out.println("Please input talent 1 of contestant 1.");
         contestants[0].addTalent(sc.next()); 
-        System.out.println("Please input talent 2 of contestant 2.");
+        System.out.println("Please input talent 2 of contestant 1.");
         contestants[0].addTalent(sc.next());
 
         if(contestants[0].canCompete("singing")){
             canCompete = true;
         }
 
-        if(contestants[0].canCompete())
+        if(contestants[0].canCompete(25)){
+            System.out.println("Can contestant 1 compete in singing : " + canCompete);
+        }
 
         contestants = null;
         sc.close();
